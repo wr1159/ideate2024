d03ef8ecdeaee8d1ddc40731c52d474bbbbc5e44
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-07-15 21:47:28 +0800

    Add Prizes

--- src/components/Sponsor.tsx
+++ src/components/Sponsor.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Sponsor: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-7xl lg:pt-16 pt-8 min-h-[50vh] gap-y-8 px-4 lg:px-8">
+    <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-7xl lg:pt-16 pt-8 pb-12 min-h-[50vh] gap-y-8 px-4 lg:px-8">
       <p className="uppercase pb-8 lg:pb-16">With Support From</p>
       {/* TODO: Add Carousel */}
       <div className='overflow-none px-16 flex gap-x-4'>
