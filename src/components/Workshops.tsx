fcf4fb1a4083244c6df4409b2d2390b0df85b838
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-07-17 03:06:24 +0800

    Clean up timeline

--- src/components/Workshops.tsx
+++ src/components/Workshops.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import TimelineItem from "./TimelineItem";
 
-const WhatIsIdeate: React.FC = () => {
+const Workshops: React.FC = () => {
   return (
-    <div className="flex flex-col text-lg xs:text-2xl lg:text-3xl xl:text-5xl lg:pt-16 pt-8 px-4 lg:px-16 leading-tight">
+    <div className="flex flex-col text-lg xs:text-2xl lg:text-3xl xl:text-5xl lg:pt-16 pt-8 px-2 lg:px-16 leading-tight">
       <p className="uppercase pb-8 lg:pb-16 align-middle mx-auto">
         Workshops
       </p>
       <div className="relative flex flex-col w-full items-center justify-center">
         <div className="absolute inset-y-0 left-1/2 transform -translate-x-1/2 w-1 bg-white z-10"></div>
-        <div className="z-20 p-8">
+        <div className="z-20 p-2 lg:p-8">
           <TimelineItem title='CAD (Solidworks)' date='21 Aug 2024' direction='left' shape='triangle' />
           <TimelineItem title='C++' date='22 Aug 2024' direction='right' shape='square' />
           <TimelineItem title='Arduino Workshop' date='23 Aug 2024' direction='left' shape='circle' />
@@ -25,4 +25,4 @@ const WhatIsIdeate: React.FC = () => {
   )
 };
 
-export default WhatIsIdeate;
+export default Workshops;
