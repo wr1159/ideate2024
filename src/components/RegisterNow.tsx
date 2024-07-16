464319d35f7c0433f8c55c1ff916dd05983c23b2
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-07-17 01:48:23 +0800

    Update Register Page with Tse Han fixes

--- src/components/RegisterNow.tsx
+++ src/components/RegisterNow.tsx
@@ -5,7 +5,7 @@ const Sponsor: React.FC = () => {
     <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-7xl lg:pt-16 pt-8 pb-12 min-h-[50vh] gap-y-8 px-4 lg:px-8">
       <a
         className="items-center flex justify-center py-16 lg:py-28 group"
-        href="https://instagram.com/nusidp"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeuCOJHRSzdpyS-YuUZS0kTl_YUQGG8eLMc9t0fHQrn9va7Wg/viewform?usp=sf_link"
         target="_blank"
       >
         <img
@@ -13,12 +13,12 @@ const Sponsor: React.FC = () => {
           alt="Lit Bulb Head"
           className="h-48 lg:min-h-72 grayscale group-hover:grayscale-0 absolute transition-all"
         />
-        <p className="z-40 lg:text-3xl text-base">Register Now</p>
+        <p className="z-40 lg:text-3xl text-base">Register Here</p>
       </a>
       <img
         src="../registration/bulb_screw.png"
         alt="Bulb Screw"
-        className="h-24 lg:h-48"
+        className="h-16 lg:h-28"
       />
       <p className="uppercase pb-8 lg:pb-16">See You There</p>
     </div>
