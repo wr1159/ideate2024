00cd1b847f658dad375efd14c3e6d0a58787cb5e
adoorknob <quantengwai@gmail.com>
2024-07-17 22:18:07 +0800

    Fix timeline sizing

--- src/components/Timeline_2.tsx
+++ src/components/Timeline_2.tsx
@@ -3,11 +3,11 @@ import TimelineItem from "./TimelineItem";
 
 const WhatIsIdeate: React.FC = () => {
   return (
-    <div className="flex flex-col text-lg xs:text-2xl lg:text-4xl xl:text-6xl lg:pt-16 pt-8 px-4 lg:px-16 leading-tight">
+    <div className="flex flex-col items-center text-lg xs:text-2xl lg:text-4xl xl:text-6xl lg:pt-16 pt-8 px-4 lg:px-16 leading-tight">
       <p className="uppercase pb-8 lg:pb-16 align-middle mx-auto">
         What to Expect
       </p>
-      <div className="relative flex flex-col w-full items-center justify-center">
+      <div className="relative flex flex-col w-3/4 items-center justify-center">
         <div className="absolute inset-y-0 left-1/2 transform -translate-x-1/2 w-1 bg-white z-10"></div>
         <div className="z-20 p-2 lg:p-8">
           <TimelineItem 
--- src/components/Workshops.tsx
+++ src/components/Workshops.tsx
@@ -3,13 +3,14 @@ import TimelineItem from "./TimelineItem";
 
 const Workshops: React.FC = () => {
   return (
-    <div className="flex flex-col text-lg xs:text-2xl lg:text-3xl xl:text-5xl lg:pt-16 pt-8 px-2 lg:px-16 leading-tight">
+    <div className="flex flex-col items-center text-lg xs:text-2xl lg:text-3xl xl:text-5xl lg:pt-16 pt-8 px-2 lg:px-16 leading-tight">
       <p className="uppercase pb-8 lg:pb-16 align-middle mx-auto">
         Workshops
       </p>
-      <div className="relative flex flex-col w-full items-center justify-center">
+      <div className="relative flex flex-col items-center">
+      <div className="absolute inset-0 bg-white z-0 rounded-md"></div>
+      <div className="relative flex flex-col w-3/4 items-center justify-center">
         <div className="absolute h-full left-1/2 transform -translate-x-1/2 w-1 bg-[#1C34D4] z-20" />
-        <div className="absolute inset-0 bg-white z-0 rounded-md"></div>
         <div className="z-30 p-2 lg:p-8">
           <TimelineItem 
             title={<p className="text-sm xs:text-lg sm:text-xl lg:text-4xl">CAD (Solidworks)</p>} 
@@ -66,6 +67,7 @@ const Workshops: React.FC = () => {
             textColor='blue'
             />
         </div>
+      </div>
       </div>
     </div>
   )
