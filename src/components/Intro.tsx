96f635aac9767c56d0be0de5d5dce0c747bf67f1
wr1159 <chuweirong@u.nus.edu>
2024-07-22 22:41:15 +0800

    Add links to IDEATE, iDP, VIP

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,9 +4,21 @@ const Intro: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-6xl lg:pt-16 pt-8 min-h-[50vh] px-4 lg:px-8">
       <p className="max-w-[75vw] place-self-start">
-        An annual makeathon organised by NUS Innovation and Design Programme (iDP) Student Club
+        An annual makeathon organised by {" "}
+        <a
+          className="underline text-gray-300 hover:text-white"
+          href="https://cde.nus.edu.sg/edic/idp/"
+          target="_blank"
+        >
+          NUS Innovation and Design Programme (iDP){" "}
+        </a>
+        Student Club
       </p>
-      <img src="../theme/bulb_with_house.png" alt="Innovation & Design Programme Students Club Logo" className="h-48 lg:-mt-10 lg:h-96 place-self-end" />
+      <img
+        src="../theme/bulb_with_house.png"
+        alt="Innovation & Design Programme Students Club Logo"
+        className="h-48 lg:-mt-10 lg:h-96 place-self-end"
+      />
     </div>
   );
 };
