d6d2ef517dd663affdaadd6bd6bcb70c1b451d31
wr1159 <chuweirong@u.nus.edu>
2024-07-22 23:05:06 +0800

    Remove iDP Link

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,13 +5,7 @@ const Intro: React.FC = () => {
     <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-6xl lg:pt-16 pt-8 min-h-[50vh] px-4 lg:px-8">
       <p className="max-w-[75vw] place-self-start">
         An annual makeathon organised by {" "}
-        <a
-          className="underline text-gray-300 hover:text-white"
-          href="https://cde.nus.edu.sg/edic/idp/"
-          target="_blank"
-        >
-          NUS Innovation and Design Programme (iDP){" "}
-        </a>
+        NUS Innovation and Design Programme (iDP){" "}
         Student Club
       </p>
       <img
