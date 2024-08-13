71e5f87344d2b6e7fb9eb990b1040472e04025f7
adoorknob <quantengwai@gmail.com>
2024-08-13 23:51:27 +0800

    close registration

--- src/components/RegisterNow.tsx
+++ src/components/RegisterNow.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Sponsor: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-7xl lg:pt-16 pt-8 pb-12 min-h-[50vh] gap-y-4 px-4 lg:px-8">
+    <div className="flex flex-col items-center justify-center gap-x-4 text-2xl lg:text-7xl lg:pt-16 pt-8 pb-12 min-h-[50vh] gap-y-8 px-4 lg:px-8">
       <a
         className="items-center flex justify-center py-16 lg:py-28 group"
-        href="https://docs.google.com/forms/d/e/1FAIpQLSeuCOJHRSzdpyS-YuUZS0kTl_YUQGG8eLMc9t0fHQrn9va7Wg/viewform?usp=sf_link"
+        // href="https://docs.google.com/forms/d/e/1FAIpQLSeuCOJHRSzdpyS-YuUZS0kTl_YUQGG8eLMc9t0fHQrn9va7Wg/viewform?usp=sf_link"
         target="_blank"
       >
         <img
@@ -14,8 +14,8 @@ const Sponsor: React.FC = () => {
           className="h-48 lg:min-h-72 lg:grayscale group-hover:grayscale-0 absolute transition-all"
         />
         <div className="flex flex-col items-center z-40">
-          <p className="z-40 lg:text-3xl text-base">Register Here</p>
-          <p className="z-40 lg:text-xl text-sm">Click Here</p>
+          <p className="z-40 lg:text-3xl text-base">Signups Closed</p>
+          {/* <p className="z-40 lg:text-xl text-sm"></p> */}
         </div>
       </a>
       <img
